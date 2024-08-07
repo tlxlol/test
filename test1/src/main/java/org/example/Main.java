82f6d492b731917158552b7efdc9fcfd4832a887
小翔哥嘎嘎 <752877329@qq.com>
2024-08-07 20:55:54 +0800

    first commit

--- test1/src/main/java/org/example/Main.java
+++ test1/src/main/java/org/example/Main.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello world! java");
         System.out.println("branch2");
+=======
+        System.out.println("Hello world!");
+        System.out.println("branch1");
+>>>>>>> Stashed changes
     }
 }
